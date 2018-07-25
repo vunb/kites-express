07e8b718267ec735fdbd24373c40fe993c42d248
Vunb <nhubaovu@gmail.com>
2018-07-25 11:30:41 +0700

    test resView callback

--- test/mixin/res.js
+++ test/mixin/res.js
@@ -23,57 +23,60 @@ test('kites views engine', (troot) => {
         discover: false
     }
 
-    // troot.test('default view engine', (t) => {
-    //     t.plan(3);
-    //     var kites = engine(config);
-    //     // config the kites-express extension
-    //     kites
-    //         .use(kitesExpress({
-    //             app: express(),
-    //             views: {
-    //                 path: path.join(__dirname, '../views')
-    //             }
-    //         }))
-    //         .init()
-    //         .then(() => {
-    //             // config express
-    //             var app = kites.express.app;
-    //             app.get('/knock', (req, res) => res.ok({
-    //                 greeting: 'Hello World!'
-    //             }));
-    
-    //             app.get('/about', (req, res) => res.view('about', {
-    //                 tagline: 'Template-based Web Application Framework!'
-    //             }, function (err) {
-    //                 console.log('ERRRROR: ', err);
-    //                 if (err) {
-    //                     t.fail(err)
-    //                 } else {
-    //                     t.false(err, 'Callback works!');
-    //                 }
-    //             }));
-    
-    //             request(app)
-    //                 .get('/knock')
-    //                 .expect(200)
-    //                 // .expect('Content-Type', /json/)
-    //                 .then((res) => {
-    //                     t.deepEqual(res.body, {
-    //                         greeting: 'Hello World!'
-    //                     }, 'res.ok([data])');
-    //                 })
-    //                 .catch(t.fail)
-    
-    //             request(app)
-    //                 .get('/about')
-    //                 // .expect('Content-Type', /html/)
-    //                 .expect(200)
-    //                 .then((res) => {
-    //                     t.equal(res.text, '<h2>About Kites</h2>\n<p>Template-based Web Application Framework!</p>\n<hr/>\n<p>Engine: <code>ejs</code></p>', 'about kites (default view engine)')
-    //                 })
-    //                 .catch(t.fail)           
-    //         })
-    // })
+    troot.test('default view engine', (t) => {
+        t.plan(3);
+
+
+        engine(config)
+            .use(kitesExpress({
+                app: express(),
+                views: {
+                    path: path.join(__dirname, '../views')
+                }
+            }))
+            .init()
+            .then((kites) => {
+                // config express
+                var app = kites.express.app;
+                app.get('/knock', (req, res) => res.ok({
+                    greeting: 'Hello World!'
+                }));
+
+                app.get('/about', (req, res) => {
+                    return res.view('about', {
+                        tagline: 'Template-based Web Application Framework!'
+                    }, function (err, renderedViewStr) {
+                        if (err) {
+                            res.serverError(err);
+                            t.fail(err);
+                        } else {
+                            res.send(renderedViewStr);
+                            t.false(err, 'Callback works!');
+                        }
+                    });
+                });
+
+                request(app)
+                    .get('/knock')
+                    .expect(200)
+                    // .expect('Content-Type', /json/)
+                    .then((res) => {
+                        t.deepEqual(res.body, {
+                            greeting: 'Hello World!'
+                        }, 'res.ok([data])');
+                    })
+                    .catch(t.fail)
+
+                request(app)
+                    .get('/about')
+                    // .expect('Content-Type', /html/)
+                    .expect(200)
+                    .then((res) => {
+                        t.equal(res.text, '<h2>About Kites</h2>\n<p>Template-based Web Application Framework!</p>\n<hr/>\n<p>Engine: <code>ejs</code></p>', 'about kites (default view engine)')
+                    })
+                    .catch(t.fail)
+            })
+    })
 
     troot.test('custom view engine', (t) => {
         t.plan(2);
@@ -98,11 +101,11 @@ test('kites views engine', (troot) => {
                 app.get('/knock', (req, res) => res.ok({
                     greeting: 'Hello World!'
                 }));
-    
+
                 app.get('/about', (req, res) => res.view('about', {
                     tagline: 'Use Handlebars View Engine!'
                 }));
-    
+
                 request(app)
                     .get('/knock')
                     .expect(200)
@@ -113,7 +116,7 @@ test('kites views engine', (troot) => {
                         }, 'res.ok([data])');
                     })
                     .catch(t.fail)
-    
+
                 request(app)
                     .get('/about')
                     .expect('Content-Type', /html/)
@@ -121,7 +124,7 @@ test('kites views engine', (troot) => {
                     .then((res) => {
                         t.equal(res.text, '<h2>About Kites</h2>\n<p>Use Handlebars View Engine!</p>\n<hr/>\n<p>Engine: <code>hbs</code></p>', 'about kites (handlebars view engine)')
                     })
-                    .catch(t.fail)           
+                    .catch(t.fail)
             })
     })
 
