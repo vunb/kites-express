4ad1d79396d02fafaa5f74c81ca577aba73e7f25
Vunb <nhubaovu@gmail.com>
2019-04-25 23:21:51 +0700

    cleanup

--- test/endTest.js
+++
@@ -1,8 +0,0 @@
-var test = require('tape');
-
-// close server by exit application
-test('Close server after testing done', function (assert) {
-    // just exit application
-    assert.end();
-    process.exit();
-});
